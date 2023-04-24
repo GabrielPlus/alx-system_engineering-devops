9f3964d6b76bf1b0bf19431f15ac01ce894007cb
gabriel <108320347+gabrielplus@users.noreply.github.com>
2023-04-24 13:24:47 +0300

    Gh

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,77 +1,35 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
+
 """
-    Uses the fake API to get an employer and export the info in csv formater
+Python script that exports data in the CSV format
 """
-import csv
-import requests
+
+from requests import get
 from sys import argv
+import csv
 
 if __name__ == "__main__":
-    id_em = argv[1]
-    url_employ = "https://jsonplaceholder.typicode.com/users/{}".format(id_em)
-    url_todos = url_employ + "/todos"
-    r_employ = requests.get(url_employ).json()
-    r_todos = requests.get(url_todos).json()
-    username = r_employ.get("username")
-    total_num_task = r_todos
-    list_report = []
-    for task in total_num_task:
-        report = {}
-        report["USER_ID"] = str(task.get("userId"))
-        report["USERNAME"] = str(username)
-        report["TASK_COMPLETED_STATUS"] = str(task.get("completed"))
-        report["TASK_TITLE"] = str(task.get("title"))
-        list_report.append(report)
-    header = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-    with open("{}.csv".format(id_em), "w") as fcsv:
-        f_csv = csv.DictWriter(fcsv, fieldnames=header, quoting=csv.QUOTE_ALL)
-        f_csv.writerows(list_report)
-=======
-"""Python script to export data in the CSV format"""
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    data = response.json()
 
-import csv
-import requests
-import sys
+    row = []
+    response2 = get('https://jsonplaceholder.typicode.com/users')
+    data2 = response2.json()
 
-base_url = 'https://jsonplaceholder.typicode.com/'
+    for i in data2:
+        if i['id'] == int(argv[1]):
+            employee = i['username']
 
+    with open(argv[1] + '.csv', 'w', newline='') as file:
+        writ = csv.writer(file, quoting=csv.QUOTE_ALL)
 
-def do_request():
-    '''Performs request'''
+        for i in data:
 
-    if not len(sys.argv):
-        return print('USAGE:', __file__, '<employee id>')
-    eid = sys.argv[1]
-    try:
-        _eid = int(sys.argv[1])
-    except ValueError:
-        return print('Employee id must be an integer')
+            row = []
+            if i['userId'] == int(argv[1]):
+                row.append(i['userId'])
+                row.append(employee)
+                row.append(i['completed'])
+                row.append(i['title'])
 
-    response = requests.get(base_url + 'users/' + eid)
-    if response.status_code == 404:
-        return print('User id not found')
-    elif response.status_code != 200:
-        return print('Error: status_code:', response.status_code)
-    user = response.json()
-
-    response = requests.get(base_url + 'todos/')
-    if response.status_code != 200:
-        return print('Error: status_code:', response.status_code)
-    todos = response.json()
-    user_todos = [todo for todo in todos
-                  if todo.get('userId') == user.get('id')]
-    completed = [todo for todo in user_todos if todo.get('completed')]
-
-    with open(eid + '.csv', 'w') as csvfile:
-        writer = csv.writer(csvfile, lineterminator='\n',
-                            quoting=csv.QUOTE_ALL)
-        [writer.writerow(['{}'.format(field) for field in
-                          (todo.get('userId'), user.get('username'),
-                           todo.get('completed'), todo.get('title'))])
-         for todo in user_todos]
-
-
-if __name__ == '__main__':
-    do_request()
->>>>>>> 5af6ec72da8ea87e9faa22c74fd05b9bea8e2dc6
+                writ.writerow(row)
