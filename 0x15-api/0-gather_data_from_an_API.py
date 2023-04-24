11fa0a32a5685349a25c930fe985a4d748b647ed
gabriel <108320347+gabrielplus@users.noreply.github.com>
2023-04-24 13:23:06 +0300

    Ty

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,69 +1,37 @@
 #!/usr/bin/python3
-python3 -c 'print(__import__("my_module").__doc__)'
+
 """
-<<<<<<< HEAD
-    Uses the fake API to get an employer
+Python script that, using a REST API, for a given employee ID,
+returns information about his/her TODO list progress.
 """
-import requests
+
+from requests import get
 from sys import argv
 
 
 if __name__ == "__main__":
-    id_em = argv[1]
-    url_employ = "https://jsonplaceholder.typicode.com/users/{}".format(id_em)
-    url_todos = url_employ + "/todos"
-    r_employ = requests.get(url_employ).json()
-    r_todos = requests.get(url_todos).json()
-    name = r_employ.get("name")
-    total_num_task = r_todos
-    done_task = [task for task in r_todos if task.get("completed")]
-    output = "Employee {} is done with tasks({}/{}):".format(
-                name, len(done_task), len(total_num_task))
-    for task in done_task:
-        output += "\n\t " + task.get("title")
-    print(output)
-=======
-    Given employee ID, returns information about his/her TODO list progress.
-"""
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    data = response.json()
+    completed = 0
+    total = 0
+    tasks = []
+    response2 = get('https://jsonplaceholder.typicode.com/users')
+    data2 = response2.json()
 
+    for i in data2:
+        if i.get('id') == int(argv[1]):
+            employee = i.get('name')
 
-import requests
-import sys
+    for i in data:
+        if i.get('userId') == int(argv[1]):
+            total += 1
 
-base_url = 'https://jsonplaceholder.typicode.com/'
+            if i.get('completed') is True:
+                completed += 1
+                tasks.append(i.get('title'))
 
+    print("Employee {} is done with tasks({}/{}):".format(employee, completed,
+                                                          total))
 
-def do_request():
-    '''Performs request'''
-    if len(sys.argv) < 2:
-        return print('USAGE:', __file__, '<employee id>')
-    eid = sys.argv[1]
-    try:
-        _eid = int(sys.argv[1])
-    except ValueError:
-        return print('Employee id must be an integer')
-
-    response = requests.get(base_url + 'users/' + eid)
-    if response.status_code == 404:
-        return print('User id not found')
-    elif response.status_code != 200:
-        return print('Error: status_code:', response.status_code)
-    user = response.json()
-
-    response = requests.get(base_url + 'todos/')
-    if response.status_code != 200:
-        return print('Error: status_code:', response.status_code)
-    todos = response.json()
-
-    user_todos = [todo for todo in todos
-                  if todo.get('userId') == user.get('id')]
-    completed = [todo for todo in user_todos if todo.get('completed')]
-    print('Employee', user.get('name'),
-          'is done with tasks({}/{}):'.
-          format(len(completed), len(user_todos)))
-    [print('\t', todo.get('title')) for todo in completed]
-
-
-if __name__ == '__main__':
-    do_request()
->>>>>>> 5af6ec72da8ea87e9faa22c74fd05b9bea8e2dc6
+    for i in tasks:
+        print("\t {}".format(i))
